51c7807a651b218fca18f6d45140d37704087117
Yaniv Brandvain <ybrandvain@gmail.com>
2015-09-01 10:13:09 -0500

    Deals with selected loci at eandom but specified positions Depends on the orginization of local folders Also deals with na values for corelations

--- scripts/admixedHelperFunctions.R
+++ scripts/admixedHelperFunctions.R
@@ -3,13 +3,15 @@ library(parallel)
 
 rm(list=ls())
 ls()
-source('~/Downloads/admixedPrimaryFunctions.R')
+source('~/Desktop/ad/admixedPrimaryFunctions.R')
+
+
 summarizeSim <- function(geno.data,trim.method, sel.loci = NA, trim.intense = .2){
   # geno.data can either be a path to a file, or can be a matrix
   if( class(geno.data)  == "character"){    
     geno.data <- as.matrix(read.csv(geno.data, sep = "\t"))/2 
   }
-  return(dim(geno.data))
+  if( nrow(geno.data) == 0){return(NULL)}
   locus.pairs <-t(combn(seq_along(geno.data[1,]),2))
   if(trim.method == "none" | trim.method == "regularR2"){    
     admixture.prop <- rowMeans(geno.data)
@@ -62,25 +64,27 @@ summarizeSim <- function(geno.data,trim.method, sel.loci = NA, trim.intense = .2
   if(is.na(sel.loci)){foc <- "none" }
   if(!is.na(sel.loci)){foc <- sapply(sel.loci, function(X){  paste(unlist(strsplit(X,".",fixed = T))[c(1,3)],collapse =" ")  }) }  
   not.our.chr.pairs <- !with(pw.sum,paste(chrom_A,chrom_B) %in% foc)
+  arb.loc <- which(with(pw.sum[not.our.chr.pairs,], chrom_A == "group4" & loc_A == 10 & chrom_B == "group5" & loc_B == 5))
   perform <- with(pw.sum[not.our.chr.pairs,],c(
-      false.pos.alpha.001 = sum(p.value < 0.001) / length(p.value),
-      false.pos.alpha.01  = sum(p.value < 0.01) / length(p.value),
-      false.pos.alpha.05  = sum(p.value < 0.05) / sum(not.our.chr.pairs),
-      quantiles = quantile(estimate),
-      focal.p = p.value[chrom_A == "group4" & loc_A == 10 & chrom_B == "group5" & loc_B == 5],
-      focal.estimate = estimate[chrom_A == "group4" & loc_A == 10 & chrom_B == "group5" & loc_B == 5]
+      false.pos.alpha.001 = sum(p.value < 0.001,na.rm=T) / sum(!is.na(p.value)),
+      false.pos.alpha.01  = sum(p.value < 0.01,na.rm=T) / sum(!is.na(p.value)),
+      false.pos.alpha.05  = sum(p.value < 0.05,na.rm=T) / sum(not.our.chr.pairs,na.rm=T),
+      quantiles = quantile(estimate,na.rm=T),
+      focal.p = ifelse(length(arb.loc)==0,NA, p.value[arb.loc]),
+      focal.estimate = ifelse(length(arb.loc)==0,NA, estimate[arb.loc])
   ))  
   if(!is.na(sel.loci)){
-      true.pos <- lapply( sel.loci , function(PAIR){   
+      true.pos <- lapply( sel.loci , function(PAIR){  
         focal.pair <- with(pw.sum, paste(chrom_A,loc_A,sep = ".")%in%PAIR & paste(chrom_B,loc_B,sep = ".")%in%PAIR)
+        if(sum(focal.pair) == 0){ return( c(p.val.rank.true.pos = NA, cor.rank.true.pos = NA))}
         with(pw.sum,
-             c( p.val.rank.true.pos = sum(p.value <= p.value[focal.pair]) , 
-                cor.rank.true.pos   = sum(abs(estimate) >= abs(estimate)[focal.pair])   ))
+             c( p.val.rank.true.pos = sum(p.value <= p.value[focal.pair],na.rm=T) , 
+                cor.rank.true.pos   = sum(abs(estimate) >= abs(estimate)[focal.pair],na.rm=T)   ))
       })
       tmp.names <- names(true.pos[[1]])
       true.pos <- Reduce(paste,true.pos)
       names(true.pos) <-  tmp.names
-      perform <- c(perform,true.pos, n.comps = nrow(pw.sum))
+      perform <- c(perform,true.pos, n.comps = sum(!is.na(pw.sum$estimate)))
     }
   return(list( perform=perform, removed = removed ))
 }
@@ -124,35 +128,31 @@ summarizeSim <- function(geno.data,trim.method, sel.loci = NA, trim.intense = .2
 
 
 setwd("~/Dropbox/LD_lowess_simulations_with_Yaniv/Admix'em_simulation_results/molly/selection")
-XXX<-sapply(list.files(),function(p){
+	sapply(list.files()[-c(1:2)],function(p){
 		runAdmixeD <- function(foc,sel.loci){
-			if(sel.loci == "XXX") {
+			if(sel.loci == "rand") {
 				temp.sel <- read.csv(paste(p, "/selected_pairs_log_",foc,sep=""),sep = "\t",stringsAsFactors=FALSE)
 				temp.sel$chr1name <- as.numeric(sapply( strsplit( gsub("chr","",temp.sel$chr1name) , "_") ,function(X){X[1]}))
 				temp.sel$chr2name <- as.numeric(sapply( strsplit( gsub("chr","",temp.sel$chr2name) , "_") ,function(X){X[1]}))
-				lapply( 1:nrow(temp.sel),function(X){
-					recover()
+				sel.loci <- lapply( 1:nrow(temp.sel),function(X){
+					with(temp.sel[X,],c( paste("group",chr1name,".",coordinate1,sep=""), paste("group",chr2name,".",coordinate2,sep=""))[order(c(chr1name,chr2name))])
 				})
 			}
 			in.file <- paste( path, foc, sep = "")
-#	  		print(in.file)
+	  		print(in.file)
 	  		approach <- list(regularR2="regularR2", none = "none", rm_focal = "rm.focal", rm_put_sel = "ancestry")
-	  		return( summarizeSim(in.file,"a",sel.loci = sel.loci ) )
-	  		sims <- lapply( approach[3:4], function(TRIM){ summarizeSim(in.file,TRIM,sel.loci = sel.loci ) })
+	  		sims <- lapply( approach, function(TRIM){ summarizeSim(in.file,TRIM,sel.loci = sel.loci ) })
 	  		list( perform = do.call(c,lapply(sims,function(X){X[[1]]})) , removed = sims$rm_put_sel$removed) 
 		}
 		path <- paste( p, "/msg_format_subsample_",sep="")
-		print(p)
-		# change here for mutlicore		 
-#		sim.sum <- mclapply(paste( path, 1:2, sep = ""), runAdmixeD,mc.cores=7)
 		sel <- ifelse( length(grep("selected_pairs_log",list.files(p))) == 0, list(c("group1.1","group2.1")),"rand")
-		maxim <- ifelse( p == "migration_parental_sel_0.5",411, 500)
-		sim.sum <- lapply(1:maxim, runAdmixeD,sel )
-		return(sim.sum)
-	#	sim.perform <- t(sapply(sim.sum,function(X){X[["perform"]]}))
-	#	IDing.sel <- sapply(sim.sum,function(X){X[["removed"]]})
-	#	write.csv( sim.perform , file = paste(p,"/","perform.csv",sep="") )
-	#	write.csv( IDing.sel , file = paste(p,"/","IDingsel.csv",sep="") )
-	#	p
+		# change here for mutlicore		 
+		sim.sum <- mclapply(1:500, runAdmixeD,sel ,mc.cores=8)
+		nulls <- sapply(sim.sum,function(Z){is.null(Z[[1]])})
+		sim.perform <- do.call(rbind,sapply(sim.sum[!nulls],function(X){X[["perform"]]},simplify=FALSE))
+		IDing.sel <- do.call(rbind,sapply(sim.sum[!nulls],function(X){X[["removed"]]},simplify=FALSE))
+		write.csv( sim.perform , file = paste(p,"/","perform.csv",sep="") )
+		write.csv( IDing.sel , file = paste(p,"/","IDingsel.csv",sep="") )
+		p
 	}
 )
